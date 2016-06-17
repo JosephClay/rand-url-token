54ca31c19286804577053e058b4646241ae89ebe
Joseph Clay <j053phclay@gmail.com>
2016-06-17 08:56:08 -0500

    add validation function

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-"use strict";
-
 const chars = require('./base64');
 const maxIndex = chars.length - 1;
 const random = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
@@ -11,4 +9,16 @@ module.exports = (length = 8) => {
         id += randomChar();
     }
     return id;
+};
+
+const rInvalidCharacters = /[^a-zA-Z0-9\-\_]/g;
+module.exports.valid = (param = '') => {
+	// check if the param is a string
+	if (typeof param !== 'string') { return false; }
+
+	// check for empty strings
+	let str = param.trim();
+	if (!str.length) { return false; }
+
+	return str === str.replace(rInvalidCharacters, '');
 };
